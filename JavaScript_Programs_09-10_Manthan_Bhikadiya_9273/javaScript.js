68c590170f1eb6e2f2ff2e7b8d44cc3b9b0df22f
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2025-08-20 17:48:45 +0530

    Programs

---
+++ JavaScript_Programs_09-10_Manthan_Bhikadiya_9273/javaScript.js
@@ -0,0 +1,61 @@
+// 1) JavaScript Program To Print Hello World 
+
+console.log("Hello World");
+
+// 2) JavaScript Program to Add Two Numbers 
+
+{
+    let x = 20;
+    let y = 30;
+    
+    console.log(x + y);
+}
+
+// 3) JavaScript Program to Find the Square Root
+
+
+{
+    let x = 2000;
+
+    console.log(x * x);
+}
+
+// 4) JavaScript Program to Calculate the Area of a Triangle
+
+{
+    let hight = 10;
+    let base = 10;
+    let Area = (hight * base)/ 2;
+
+    console.log(Area);
+}
+
+// 5) JavaScript Program to Swap Two Variables 
+
+{
+    let a=70;
+    let b=50;
+    console.log("A =",a,"|","B =",b );
+    a=a-b;
+    b=a+b;
+    a=b-a;
+    console.log("A =",a,"|","B =",b );
+}
+
+// 7) JavaScript Program to Convert Kilometres to Miles 
+
+{
+   let kilometetrs = 10;
+   let miles = kilometetrs * 0.621371
+
+   console.log(miles);
+}
+
+// 8) Javascript Program to Convert Celsius to Fahrenheit 
+
+{
+    let Celsius = 10;
+    let Fahrenheit = (Celsius * 9 / 5) + 32;
+
+    console.log(Fahrenheit);
+}
