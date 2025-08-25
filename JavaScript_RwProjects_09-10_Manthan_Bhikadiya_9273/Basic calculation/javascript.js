50665ab8b4bf9d047420b2c59084a87c5cc8262d
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2025-08-25 10:58:33 +0530

    Basic calculation

---
+++ JavaScript_RwProjects_09-10_Manthan_Bhikadiya_9273/Basic calculation/javascript.js
@@ -0,0 +1,18 @@
+let a = 12;
+let b = 110;
+
+console.log("Addition of a and b = " +(a+b));
+
+console.log("Subtraction of a and b = " + (a-b));
+
+console.log("Multiplication of a and b = " + (a*b));
+
+console.log("Exponentiation of a and b = " + (a**b));
+
+console.log("Division of a and b = " + (a/b));
+
+console.log("Modulus of a and b = " + (a%b));
+
+console.log("Increment of a and b = " + (a+ ++b));
+
+console.log("Decrement of a and b = " + (a+ --b));
